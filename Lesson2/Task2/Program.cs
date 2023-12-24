acea7507096ae7c9b48227e15d7115d1cd637f74
Stepan Ilin <stepa_ilin@mail.ru>
2023-12-24 14:32:26 +0300

    Task3

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] arr3 = new int[n];
+int i = 0;
+while (i < arr3.Length)
+{
+    arr3[i] = i + 1;
+    // Console.Write(arr3[i]);
+    // Console.Write(" ");
+    Console.Write($"{arr3[i]} ");
+    i = i + 1;
+
+
+}
---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// int n = 10;
+int[] array = { 1, 6, 4, 9, 7, 1, 4, 7, 3, 8 };
+int i = 0;
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write(array[i] + " ");
+    }
+    i++;
+}
