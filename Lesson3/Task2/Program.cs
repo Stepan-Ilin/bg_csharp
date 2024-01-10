997985c185a8247370b53823e9391c1471c5b465
Stepan Ilin <stepa_ilin@mail.ru>
2024-01-10 22:33:00 +0300

    add Lesson3:Task3

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintSquare(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+
+}
+
+PrintSquare(5);
+PrintSquare(10);
+PrintSquare(15);
---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void ZeroEvenElement(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+int[] array = { 14, 46, 8, 9, 33 };
+ZeroEvenElement(array);
+PrintArray(array);
